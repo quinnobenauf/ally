4a92a886c2baf82d444257561d7c94c8c7f8e1c3
burstein <o.burstein@f5.com>
2019-05-07 13:03:22 -0700

    adding a data creation file

---
+++ back-end/data-setup/createData.js
@@ -0,0 +1,88 @@
+// 1. if you are using a local instance of MongoDB create a folder named db in the back-end folder
+// 2. from the back-end folder, launch the mongo db: mongod -port 3000 -dbpath ".\db"
+// 3. To load data, open your mongo shell in teh back-end folder and type
+// load('data-setup/createData.js');
+// console will return 'true' if load was successful
+
+// Create Users
+
+db = db.getSiblingDB('ally')
+db.createCollection('users')
+usersCollection = db.getCollection("users")
+usersCollection.remove({})
+usersCollection.insert(
+  {
+    firstName: "Omer",
+    lastName: "Burstein",
+    userName: "otown",
+    password: "pword123",
+    email: "burstei3@seattleu.edu",
+    phone: "425-555-1212",
+    allergies: null,
+    diets: null
+  }
+)
+listsCollection.insert(
+  {
+    firstName: "Quinn",
+    lastName: "Obenauf",
+    userName: "quinn",
+    password: "pword123",
+    email: "quin@seattleu.edu",
+    phone: "425-123-4567",
+    allergies: null,
+    diets: null
+  }
+)
+listsCollection.insert(
+  {
+    firstName: "Jared",
+    lastName: "Alonzo",
+    userName: "JarJar",
+    password: "pword123",
+    email: "jared@seattleu.edu",
+    phone: "425-789-0000",
+    allergies: null,
+    diets: null
+  }
+)
+listsCollection.insert(
+  {
+    firstName: "Bridger",
+    lastName: "Zosky",
+    userName: "bridgy",
+    password: "pword123",
+    email: "bridge@seattleu.edu",
+    phone: "425-100-1999",
+    allergies: null,
+    diets: null
+  }
+)
+
+// Create Allergies
+
+db.createCollection('allergies')
+allergyCollection = db.getCollection("allergies")
+allergyCollection.remove({})
+allergyCollection.insertMany([
+  { type: "wheat" },
+  { type: "peanut" },
+  { type: "chocolate" },
+  { type: "gluten" },
+  { type: "shellfish" },
+  { type: "soy" }]
+)
+
+// Create Diets
+
+db.createCollection('diets')
+dietCollection = db.getCollection("diets")
+dietCollection.remove({})
+dietCollection.insertMany([
+  { type: "Vegetarian" },
+  { type: "Paleo" },
+  { type: "Vegan" },
+  { type: "Meatatarian" },
+  { type: "Gluten-free" },
+  { type: "Pescatarian" }]
+)
