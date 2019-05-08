70e24df3b6c82d70a59abdab6a2995067db18bfb
Jared <jared.alonzo@icloud.com>
2019-05-08 16:15:53 -0700

    added prototype

---
+++ prototype/src/app/table-list/table-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'app-table-list',
+  templateUrl: './table-list.component.html',
+  styleUrls: ['./table-list.component.css'],
+})
+export class TableListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
