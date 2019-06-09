c70f2846f99adc34ed2dfefdbce6a5cd1d6c3c1b
burstein <o.burstein@f5.com>
2019-06-09 14:45:29 -0700

    google passport files

---
+++ back-end/googleOauth2.ts
@@ -0,0 +1,6 @@
+class googleOauth2 {
+    static id: string = '13458652475-t35air23dcj59thditi99q3jkpeur4s9.apps.googleusercontent.com';
+    static secret:string = 'B7q_MLQG8WdAEWGfbyW_doWI';
+    
+    }
+    export default googleOauth2;
